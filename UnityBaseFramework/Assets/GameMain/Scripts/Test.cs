e0b3af4bc96eced12def54e60ec540f1c61cafd2
fuemocheng <fuemocheng@163.com>
2022-08-12 11:56:22 +0800

    Create Test.cs
    Create Test.cs

---
+++ UnityBaseFramework/Assets/GameMain/Scripts/Test.cs
@@ -0,0 +1,24 @@
+// ------------------------------------------------------------
+// FILENAME:        Test.cs
+// AUTHOR:          xgc
+// VERSION:         v1.0
+// DESCRIPTION:     
+// ------------------------------------------------------------
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
