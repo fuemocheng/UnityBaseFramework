3cde357d396355c445740ff3ccd43ccc34500754
fuemocheng <fuemocheng@163.com>
2023-07-04 10:44:00 +0800

    Add ResourceCollection configs, ResourceEditor configs
    Add ResourceCollection configs, ResourceEditor configs

---
+++ UnityBaseFramework/Assets/GameMain/Scripts/Editor/BaseFrameworkConfigs.cs
@@ -0,0 +1,23 @@
+using BaseFramework;
+using System.IO;
+using UnityEngine;
+using UnityBaseFramework.Editor;
+using UnityBaseFramework.Editor.ResourceTools;
+
+namespace XGame.Editor
+{
+    public static class GameFrameworkConfigs
+    {
+        //[BuildSettingsConfigPath]
+        //public static string BuildSettingsConfig = Utility.Path.GetRegularPath(Path.Combine(Application.dataPath, "GameMain/Configs/BuildSettings.xml"));
+
+        [ResourceCollectionConfigPath]
+        public static string ResourceCollectionConfig = Utility.Path.GetRegularPath(Path.Combine(Application.dataPath, "GameMain/Configs/ResourceCollection.xml"));
+
+        [ResourceEditorConfigPath]
+        public static string ResourceEditorConfig = Utility.Path.GetRegularPath(Path.Combine(Application.dataPath, "GameMain/Configs/ResourceEditor.xml"));
+
+        //[ResourceBuilderConfigPath]
+        //public static string ResourceBuilderConfig = Utility.Path.GetRegularPath(Path.Combine(Application.dataPath, "GameMain/Configs/ResourceBuilder.xml"));
+    }
+}
