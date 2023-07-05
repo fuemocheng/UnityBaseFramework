f72f50386ce78fdb30943577a4007793e0483cef
fuemocheng <fuemocheng@163.com>
2023-07-05 15:58:33 +0800

    Add ComponentInspector
    Add ComponentInspector;

---
+++ UnityBaseFramework/Assets/BaseFramework/Scripts/Editor/Inspector/SceneComponentInspector.cs
@@ -0,0 +1,68 @@
+using UnityEditor;
+using UnityEngine;
+using UnityBaseFramework.Runtime;
+
+namespace UnityBaseFramework.Editor
+{
+    [CustomEditor(typeof(SceneComponent))]
+    internal sealed class SceneComponentInspector : BaseFrameworkInspector
+    {
+        private SerializedProperty m_EnableLoadSceneUpdateEvent = null;
+        private SerializedProperty m_EnableLoadSceneDependencyAssetEvent = null;
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            serializedObject.Update();
+
+            SceneComponent t = (SceneComponent)target;
+
+            EditorGUI.BeginDisabledGroup(EditorApplication.isPlayingOrWillChangePlaymode);
+            {
+                EditorGUILayout.PropertyField(m_EnableLoadSceneUpdateEvent);
+                EditorGUILayout.PropertyField(m_EnableLoadSceneDependencyAssetEvent);
+            }
+            EditorGUI.EndDisabledGroup();
+
+            serializedObject.ApplyModifiedProperties();
+
+            if (EditorApplication.isPlaying && IsPrefabInHierarchy(t.gameObject))
+            {
+                EditorGUILayout.LabelField("Loaded Scene Asset Names", GetSceneNameString(t.GetLoadedSceneAssetNames()));
+                EditorGUILayout.LabelField("Loading Scene Asset Names", GetSceneNameString(t.GetLoadingSceneAssetNames()));
+                EditorGUILayout.LabelField("Unloading Scene Asset Names", GetSceneNameString(t.GetUnloadingSceneAssetNames()));
+                EditorGUILayout.ObjectField("Main Camera", t.MainCamera, typeof(Camera), true);
+
+                Repaint();
+            }
+        }
+
+        private void OnEnable()
+        {
+            m_EnableLoadSceneUpdateEvent = serializedObject.FindProperty("m_EnableLoadSceneUpdateEvent");
+            m_EnableLoadSceneDependencyAssetEvent = serializedObject.FindProperty("m_EnableLoadSceneDependencyAssetEvent");
+        }
+
+        private string GetSceneNameString(string[] sceneAssetNames)
+        {
+            if (sceneAssetNames == null || sceneAssetNames.Length <= 0)
+            {
+                return "<Empty>";
+            }
+
+            string sceneNameString = string.Empty;
+            foreach (string sceneAssetName in sceneAssetNames)
+            {
+                if (!string.IsNullOrEmpty(sceneNameString))
+                {
+                    sceneNameString += ", ";
+                }
+
+                sceneNameString += SceneComponent.GetSceneName(sceneAssetName);
+            }
+
+            return sceneNameString;
+        }
+    }
+}
