a548b2b44de7ce17d6e060ec1790e034d438a9e8
fuemocheng <fuemocheng@163.com>
2023-07-05 16:02:36 +0800

    Add ResourceBuilder
    Add ResourceBuilder;

---
+++ UnityBaseFramework/Assets/BaseFramework/Scripts/Editor/ResourceBuilder/ResourceBuilderConfigPathAttribute.cs
@@ -0,0 +1,9 @@
+namespace UnityBaseFramework.Editor.ResourceTools
+{
+    /// <summary>
+    /// ResourceBuilder 配置路径属性。
+    /// </summary>
+    public sealed class ResourceBuilderConfigPathAttribute : ConfigPathAttribute
+    {
+    }
+}
---
+++ UnityBaseFramework/Assets/BaseFramework/Scripts/Editor/ResourceBuilder/ResourceBuilderController.FileSystemHelper.cs
@@ -0,0 +1,15 @@
+using BaseFramework.FileSystem;
+
+namespace UnityBaseFramework.Editor.ResourceTools
+{
+    public sealed partial class ResourceBuilderController
+    {
+        private sealed class FileSystemHelper : IFileSystemHelper
+        {
+            public FileSystemStream CreateFileSystemStream(string fullPath, FileSystemAccess access, bool createNew)
+            {
+                return new CommonFileSystemStream(fullPath, access, createNew);
+            }
+        }
+    }
+}
