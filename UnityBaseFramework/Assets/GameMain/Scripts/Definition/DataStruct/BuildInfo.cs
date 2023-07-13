632a225f28933d869dc70481b589b02e583953d8
fuemocheng <fuemocheng@163.com>
2023-07-13 16:38:17 +0800

    Add Definition
    Add Definition, Constant, DataStruct;

---
+++ UnityBaseFramework/Assets/GameMain/Scripts/Definition/DataStruct/BuildInfo.cs
@@ -0,0 +1,47 @@
+namespace XGame
+{
+    public class BuildInfo
+    {
+        public string GameVersion
+        {
+            get;
+            set;
+        }
+
+        public int InternalGameVersion
+        {
+            get;
+            set;
+        }
+
+        public string CheckVersionUrl
+        {
+            get;
+            set;
+        }
+
+        public string WindowsAppUrl
+        {
+            get;
+            set;
+        }
+
+        public string MacOSAppUrl
+        {
+            get;
+            set;
+        }
+
+        public string IOSAppUrl
+        {
+            get;
+            set;
+        }
+
+        public string AndroidAppUrl
+        {
+            get;
+            set;
+        }
+    }
+}
