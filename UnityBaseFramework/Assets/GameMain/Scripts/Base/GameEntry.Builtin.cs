6a35e44178318ccb30fda3e021bdc5f6b5eddcc1
fuemocheng <fuemocheng@163.com>
2023-07-13 21:54:45 +0800

    Update GameEntry.Builtin.cs

--- UnityBaseFramework/Assets/GameMain/Scripts/Base/GameEntry.Builtin.cs
+++ UnityBaseFramework/Assets/GameMain/Scripts/Base/GameEntry.Builtin.cs
@@ -20,29 +20,29 @@ namespace XGame
         /// <summary>
         /// 获取配置组件。
         /// </summary>
-        //public static ConfigComponent Config
-        //{
-        //    get;
-        //    private set;
-        //}
+        public static ConfigComponent Config
+        {
+            get;
+            private set;
+        }
 
         /// <summary>
         /// 获取数据结点组件。
         /// </summary>
-        //public static DataNodeComponent DataNode
-        //{
-        //    get;
-        //    private set;
-        //}
+        public static DataNodeComponent DataNode
+        {
+            get;
+            private set;
+        }
 
         /// <summary>
         /// 获取数据表组件。
         /// </summary>
-        //public static DataTableComponent DataTable
-        //{
-        //    get;
-        //    private set;
-        //}
+        public static DataTableComponent DataTable
+        {
+            get;
+            private set;
+        }
 
         /// <summary>
         /// 获取调试组件。
@@ -56,11 +56,11 @@ namespace XGame
         /// <summary>
         /// 获取下载组件。
         /// </summary>
-        //public static DownloadComponent Download
-        //{
-        //    get;
-        //    private set;
-        //}
+        public static DownloadComponent Download
+        {
+            get;
+            private set;
+        }
 
         /// <summary>
         /// 获取实体组件。
@@ -83,20 +83,20 @@ namespace XGame
         /// <summary>
         /// 获取文件系统组件。
         /// </summary>
-        //public static FileSystemComponent FileSystem
-        //{
-        //    get;
-        //    private set;
-        //}
+        public static FileSystemComponent FileSystem
+        {
+            get;
+            private set;
+        }
 
         /// <summary>
         /// 获取有限状态机组件。
         /// </summary>
-        //public static FsmComponent Fsm
-        //{
-        //    get;
-        //    private set;
-        //}
+        public static FsmComponent Fsm
+        {
+            get;
+            private set;
+        }
 
         /// <summary>
         /// 获取本地化组件。
@@ -119,47 +119,47 @@ namespace XGame
         /// <summary>
         /// 获取对象池组件。
         /// </summary>
-        //public static ObjectPoolComponent ObjectPool
-        //{
-        //    get;
-        //    private set;
-        //}
+        public static ObjectPoolComponent ObjectPool
+        {
+            get;
+            private set;
+        }
 
         /// <summary>
         /// 获取流程组件。
         /// </summary>
-        //public static ProcedureComponent Procedure
-        //{
-        //    get;
-        //    private set;
-        //}
+        public static ProcedureComponent Procedure
+        {
+            get;
+            private set;
+        }
 
         /// <summary>
         /// 获取资源组件。
         /// </summary>
-        //public static ResourceComponent Resource
-        //{
-        //    get;
-        //    private set;
-        //}
+        public static ResourceComponent Resource
+        {
+            get;
+            private set;
+        }
 
         /// <summary>
         /// 获取场景组件。
         /// </summary>
-        //public static SceneComponent Scene
-        //{
-        //    get;
-        //    private set;
-        //}
+        public static SceneComponent Scene
+        {
+            get;
+            private set;
+        }
 
         /// <summary>
         /// 获取配置组件。
         /// </summary>
-        //public static SettingComponent Setting
-        //{
-        //    get;
-        //    private set;
-        //}
+        public static SettingComponent Setting
+        {
+            get;
+            private set;
+        }
 
         /// <summary>
         /// 获取声音组件。
@@ -191,22 +191,22 @@ namespace XGame
         private static void InitBuiltinComponents()
         {
             Base = UnityBaseFramework.Runtime.BaseEntry.GetComponent<BaseComponent>();
-            //Config = UnityBaseFramework.Runtime.BaseEntry.GetComponent<ConfigComponent>();
-            //DataNode = UnityBaseFramework.Runtime.BaseEntry.GetComponent<DataNodeComponent>();
-            //DataTable = UnityBaseFramework.Runtime.BaseEntry.GetComponent<DataTableComponent>();
+            Config = UnityBaseFramework.Runtime.BaseEntry.GetComponent<ConfigComponent>();
+            DataNode = UnityBaseFramework.Runtime.BaseEntry.GetComponent<DataNodeComponent>();
+            DataTable = UnityBaseFramework.Runtime.BaseEntry.GetComponent<DataTableComponent>();
             //Debugger = UnityBaseFramework.Runtime.BaseEntry.GetComponent<DebuggerComponent>();
-            //Download = UnityBaseFramework.Runtime.BaseEntry.GetComponent<DownloadComponent>();
+            Download = UnityBaseFramework.Runtime.BaseEntry.GetComponent<DownloadComponent>();
             //Entity = UnityBaseFramework.Runtime.BaseEntry.GetComponent<EntityComponent>();
             Event = UnityBaseFramework.Runtime.BaseEntry.GetComponent<EventComponent>();
-            //FileSystem = UnityBaseFramework.Runtime.BaseEntry.GetComponent<FileSystemComponent>();
-            //Fsm = UnityBaseFramework.Runtime.BaseEntry.GetComponent<FsmComponent>();
+            FileSystem = UnityBaseFramework.Runtime.BaseEntry.GetComponent<FileSystemComponent>();
+            Fsm = UnityBaseFramework.Runtime.BaseEntry.GetComponent<FsmComponent>();
             //Localization = UnityBaseFramework.Runtime.BaseEntry.GetComponent<LocalizationComponent>();
             //Network = UnityBaseFramework.Runtime.BaseEntry.GetComponent<NetworkComponent>();
-            //ObjectPool = UnityBaseFramework.Runtime.BaseEntry.GetComponent<ObjectPoolComponent>();
-            //Procedure = UnityBaseFramework.Runtime.BaseEntry.GetComponent<ProcedureComponent>();
-            //Resource = UnityBaseFramework.Runtime.BaseEntry.GetComponent<ResourceComponent>();
-            //Scene = UnityBaseFramework.Runtime.BaseEntry.GetComponent<SceneComponent>();
-            //Setting = UnityBaseFramework.Runtime.BaseEntry.GetComponent<SettingComponent>();
+            ObjectPool = UnityBaseFramework.Runtime.BaseEntry.GetComponent<ObjectPoolComponent>();
+            Procedure = UnityBaseFramework.Runtime.BaseEntry.GetComponent<ProcedureComponent>();
+            Resource = UnityBaseFramework.Runtime.BaseEntry.GetComponent<ResourceComponent>();
+            Scene = UnityBaseFramework.Runtime.BaseEntry.GetComponent<SceneComponent>();
+            Setting = UnityBaseFramework.Runtime.BaseEntry.GetComponent<SettingComponent>();
             //Sound = UnityBaseFramework.Runtime.BaseEntry.GetComponent<SoundComponent>();
             //UI = UnityBaseFramework.Runtime.BaseEntry.GetComponent<UIComponent>();
             WebRequest = UnityBaseFramework.Runtime.BaseEntry.GetComponent<WebRequestComponent>();
