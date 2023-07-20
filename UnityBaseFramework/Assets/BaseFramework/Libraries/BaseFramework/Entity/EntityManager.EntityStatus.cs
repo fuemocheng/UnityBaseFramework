c060f53f200a431d32a5c9e5c89265fdcf8fe691
fuemocheng <fuemocheng@163.com>
2023-07-20 11:55:43 +0800

    Add Entity
    Add Entity;

---
+++ UnityBaseFramework/Assets/BaseFramework/Libraries/BaseFramework/Entity/EntityManager.EntityStatus.cs
@@ -0,0 +1,21 @@
+namespace BaseFramework.Entity
+{
+    internal sealed partial class EntityManager : BaseFrameworkModule, IEntityManager
+    {
+        /// <summary>
+        /// 实体状态。
+        /// </summary>
+        private enum EntityStatus : byte
+        {
+            Unknown = 0,
+            WillInit,
+            Inited,
+            WillShow,
+            Showed,
+            WillHide,
+            Hidden,
+            WillRecycle,
+            Recycled
+        }
+    }
+}
---
+++ UnityBaseFramework/Assets/BaseFramework/Scripts/Runtime/Entity/EntityGroupHelperBase.cs
@@ -0,0 +1,12 @@
+using BaseFramework.Entity;
+using UnityEngine;
+
+namespace UnityBaseFramework.Runtime
+{
+    /// <summary>
+    /// 实体组辅助器基类。
+    /// </summary>
+    public abstract class EntityGroupHelperBase : MonoBehaviour, IEntityGroupHelper
+    {
+    }
+}
