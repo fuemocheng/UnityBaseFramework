d1dc831cc4b8c4997045cef49ead8ee34adc4a53
Hunt <fuemocheng@163.com>
2023-09-09 23:08:43 +0800

    Add Variable
    Add Variable

---
+++ UnityBaseFramework/Assets/BaseFramework/Scripts/Runtime/Variable/VarObject.cs
@@ -0,0 +1,17 @@
+using BaseFramework;
+
+namespace UnityBaseFramework.Runtime
+{
+    /// <summary>
+    /// System.Object 变量类。
+    /// </summary>
+    public sealed class VarObject : Variable<object>
+    {
+        /// <summary>
+        /// 初始化 System.Object 变量类的新实例。
+        /// </summary>
+        public VarObject()
+        {
+        }
+    }
+}
